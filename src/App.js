13218ed6237ec803ba596efdeb6e212469ab5eeb
jason <2B3>
2021-09-09 17:52:28 -0400

    prepared to start transition to Method 2

--- src/App.js
+++ src/App.js
@@ -4,11 +4,12 @@ import { uiActions } from "./store/ui-slice"; //
 import Cart from "./components/Cart/Cart";
 import Layout from "./components/Layout/Layout";
 import Products from "./components/Shop/Products";
-import Notification from "./components/UI/Notification"
+import Notification from "./components/UI/Notification";
 let isInitial = true;
 
 function App() {
   const dispatch = useDispatch();
+
   // A helper function to help us a call one of ui-slice.js reducer functions
   // Will be used to render content when request is pending, successful, or a failure
   function uiSlice_showNotification(status, title, message) {
@@ -19,11 +20,10 @@ function App() {
         message: message,
     }))
   }
-
-  const showCart = useSelector((state) => state.ui.cartIsVisible);
-  // Accessing cartSlice's initialState: { items: [], totalQuantity: 0 }
-  const cart = useSelector((state) => state.cart);
-  const notification = useSelector((state) => state.ui.notification);
+  // Access Redux store variables (check the slice files)
+  const showCart = useSelector((state) => state.ui.cartIsVisible); // ui-slice.js
+  const cart = useSelector((state) => state.cart); // cart-slice.js
+  const notification = useSelector((state) => state.ui.notification); // ui-slice.js
 
   useEffect(() => {
     if (isInitial) {
@@ -33,43 +33,43 @@ function App() {
     console.log("useEffect ran!");
 
     const sendCartData = async function () {
-      //% showNotification: Pending state
+      // showNotification: Pending state
       uiSlice_showNotification("pending", "sending", "Sending cart data!");
 
       //% Update Firebase data
       // prettier-ignore
-      const response= await fetch("https://reduxadv-c13f7-default-rtdb.firebaseio.com/data.jsn", {
+      const response= await fetch("https://reduxadv-c13f7-default-rtdb.firebaseio.com/data.json", {
         method: "PUT", // replace data in our database
         body: JSON.stringify(cart),
       });
       if (!response.ok) throw new Error("Sending cart data failed");
 
-      //% showNotification: Success state
+      // showNotification: Success state
       uiSlice_showNotification("success", "Success", "Data sent successfully!");
     }; // END OF ASYNC FUNCTION
 
-    // Invoke your async function within useEffect, then catch any errors
-    // Alternative to the try/catch method
+    // Invoke your async function within useEffect, then catch any errors (alt try/catch)
     sendCartData().catch((error) => {
       uiSlice_showNotification("error", "Error", "Sending data failed");
+      console.log("oopsie");
     });
     // ----- end of useEffect ------------
   }, [cart]);
   // this re-executes when Redux store's item KVP gets updated via a dispatched action
   return (
     <>
-    {notification && (
+      {notification && (
         <Notification
           status={notification.status}
           title={notification.title}
           message={notification.message}
         />
       )}
-    <Layout>
-      {showCart && <Cart />}
-      <Products />
-    </Layout>
-  </>
+      <Layout>
+        {showCart && <Cart />}
+        <Products />
+      </Layout>
+    </>
   );
 }
 
